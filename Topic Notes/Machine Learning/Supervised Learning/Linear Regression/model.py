c0fcf113d9f7c6d9be6afc548d0f287c540cf504
Ryan Johnson <rtjohnson1206@gmail.com>
2024-10-28 14:08:38 -0700

    :card_file_box: vault backup: 2024-10-28

--- Topic Notes/Machine Learning/Supervised Learning/Linear Regression/model.py
+++
@@ -1,9 +0,0 @@
-# Model representation
-# y = β0 + β1 x1 + β2 x2 + ... + βn xn + ϵ
-
-# Model assumptions
-# - Linearity
-# - Independence of errors
-# - Homoscedasticity (Constant variance of errors)
-# - Normality of errors
-# - No or little Multicollinearity
